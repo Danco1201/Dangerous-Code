4b7d3bee93e769abd40db3b011a9f790e2f34ffc
Danco1201 <fr4nte.del.sur@gmail.com>
2025-02-21 15:44:56 -0300

    Create unnecesarythings.c

---
+++ inoffensive/c/unnecesarythings.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define x int main {
+#define xx "H"
+#define xxx "e"
+#define xxxx "l"
+#define xxxxx "l"
+#define xxxxxx "o"
+#define xxxxxxx ","
+#define xxxxxxxx " "
+#define xxxxxxxxx "W"
+#define xxxxxxxxxx "o"
+#define xxxxxxxxxxx "r"
+#define xxxxxxxxxxxx "l"
+#define xxxxxxxxxxxxx "d"
+#define xxxxxxxxxxxxxx "!"
+#define xxxxxxxxxxxxxxx printf
+#define xxxxxxxxxxxxxxxx return 0; }
+ 
+x 
+  xxxxxxxxxxxxxxx(xx xxx xxxx xxxxx xxxxxx xxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxxxxx);
+  xxxxxxxxxxxxxxxx
