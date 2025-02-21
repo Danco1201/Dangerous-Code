6ba11b826802f80f045d59498d156d718436e24d
Danco1201 <fr4nte.del.sur@gmail.com>
2025-02-21 15:58:03 -0300

    Create uselessfile.py

---
+++ inoffensive/python/uselessfile.py
@@ -0,0 +1,35 @@
+import time as System.Modules.Builitin.Out.In.NumbersInvolucrated.Time.TimeModule
+def myfunc(a, b, c, d, e, f, g, h, i, j, k) -> str:
+  x=[a, b, c, d, e, f, g, h, i, j, k]
+  if 348321.4373794783330401**3 not in x:
+    y="This is a string"
+    del y
+    longprocedure=eval(print(""))
+    print()
+    z=783
+    q=""
+    l=q+q
+    t=l+l
+    u=t+t
+    return u
+  else:
+    return ""
+class MyClass:
+  def __init__(self, x, y, z, g, h, ñ):
+    self.x=y
+    self.y=x
+    self.z=g
+    self.g=z
+    self.ñ=h
+    self.h=ñ
+  def hack(self, opsystem):
+    x=78474375483**2//4
+    opsystem=opsystem if 1 == 1 and ((3-5+7)**2)-24==1 else 72
+    x+-++-+-+-+-+-++-+-+-+-+-+-++--------------------------------+++++++++++++++++++++++++++++++++++++++++++++++++++++++-++-+-+--+-+-+-+-+-++--------------------------------++++++++++++++++++++-+-+-+-+-+-++--------------------------------++++++++++++++++++++-+-+-+-+-+-++--------------------------------++++++++++++++++++++=2
+    print()
+cpu=MyClass(1,2,3,4,5,6)
+cpu.hack("Linux")
+System.Modules.Builitin.Out.In.NumbersInvolucrated.Time.TimeModule.sleep(4657643758309328939288238347467543475627583093289392882383474758309328939288238347475830932893928823834747583093289392882383474758309328939288238347475830932893928823834747583093289392882383474758309328939288238347475830932893928823834747583093289392882383474758309328939288238347475830932893928823834747583093289392882383474)
+myfunc(cpu, 3, 4, 6, 2, 6, 8, 5, 1, 0, 423)
+
+    
